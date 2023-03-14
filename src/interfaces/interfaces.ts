c7741fb1d326c1c76a6fa6f71d90c9a17f0dd4ce
Vis <visu4lny@gmail.com>
2023-03-14 17:29:21 +0100

    Add custom Typescript interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,45 @@
+export interface TodoList {
+  listItems: Array<ListItem>;
+};
+
+export interface DeletedList {
+  listItems: Array<DeletedListItem>;
+};
+
+export interface KanbanBoard {
+  columns: Array<BoardColumn>;
+};
+
+export interface Command {
+  command: string | string[] | RegExp;
+  callback: (...args: any[]) => unknown;
+  isFuzzyMatch?: boolean | undefined;
+  matchInterim?: boolean | undefined;
+  fuzzyMatchingThreshold?: number | undefined;
+  bestMatchOnly?: boolean | undefined;
+}
+
+interface ListItem {
+  id: string;
+  value: string;
+  isFinished: boolean;
+  date: string;
+};
+
+interface DeletedListItem {
+  id: string;
+  value: string;
+  deletedDate: string;
+};
+
+interface BoardColumn {
+  id: string;
+  title: string;
+  cards: Array<Card>;
+};
+
+interface Card {
+  id: string;
+  value: string;
+  date: string;
+};
