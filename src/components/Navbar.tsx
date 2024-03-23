75533989a3c969c947d933861b5f038f2407e4f2
Visu4lny <visu4lny@gmail.com>
2024-03-23 10:21:28 +0100

    Fix styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,49 @@
 import React from "react";
-import CSS from 'csstype';
+import CSS from "csstype";
 
 type NavbarProps = {
-  currentModule: string,
-  handleSwitchModule: (module: string) => void,
-  navStyle: Array<CSS.Properties>
+  currentModule: string;
+  handleSwitchModule: (module: string) => void;
 };
 
 export default function Navbar(props: NavbarProps) {
+  const navStyle: Array<CSS.Properties> = [
+    {
+      width: "200px",
+      height: "50px",
+      color: "#FFFFFF",
+      backgroundColor: "#445493",
+      border: "none",
+      borderRadius: "5px",
+      fontSize: "1.2em",
+    },
+    {
+      width: "200px",
+      height: "50px",
+      color: "#A5A5A5",
+      backgroundColor: "#1F273D",
+      border: "none",
+      borderRadius: "5px",
+      fontSize: "1.2em",
+    },
+  ];
+
   return (
     <div className="navbar">
-      <button className="todoModule" style={props.navStyle[0]} onClick={() => props.handleSwitchModule("todo")}>Todo Module</button>
-      <button className="kanbanModule" style={props.navStyle[1]} onClick={() => props.handleSwitchModule("kanban")}>Kanban Module</button>
+      <button
+        className="todoModule"
+        style={props.currentModule === "todo" ? navStyle[0] : navStyle[1]}
+        onClick={() => props.handleSwitchModule("todo")}
+      >
+        Todo Module
+      </button>
+      <button
+        className="kanbanModule"
+        style={props.currentModule === "kanban" ? navStyle[0] : navStyle[1]}
+        onClick={() => props.handleSwitchModule("kanban")}
+      >
+        Kanban Module
+      </button>
     </div>
   );
-};
+}
